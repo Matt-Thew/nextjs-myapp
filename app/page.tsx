32bb2a8dcd68fd117aa06a5f47e1acd88e97e7ba
Noppadon <noppadon.kh@indexlivingmall.com>
2025-01-26 14:00:51 +0700

    update @

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
-import Carousel from "../components/Carousel";
-import Header from "../components/Header";
-import ContactUs from "../components/home/ContactUs";
-import Footer from "../components/home/Footer";
-import Intro from "../components/home/Intro";
-import BlogSection from "../components/home/News";
-import ParallaxEffect from "../components/home/ParallaxEffect";
-import Services from "../components/home/Services";
-import Team from "../components/home/Team";
+// import Carousel from "../components/Carousel";
+// import Header from "../components/Header";
+// import ContactUs from "../components/home/ContactUs";
+// import Footer from "../components/home/Footer";
+// import Intro from "../components/home/Intro";
+// import BlogSection from "../components/home/News";
+// import ParallaxEffect from "../components/home/ParallaxEffect";
+// import Services from "../components/home/Services";
+// import Team from "../components/home/Team";
 
 export default function Home() {
   return (
     <>
-      <Header />
+      Test
+      {/* <Header /> */}
       {/* <Carousel />
       <Intro />
       <Team />
