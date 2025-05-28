9394331f0fb6366a81e43865a6c1dfaaeeb9c325
Noppadon <noppadon.kh@indexlivingmall.com>
2025-05-28 16:08:44 +0700

    update maintenance message in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,18 @@ import MyTeam from "@/components/home/MyTeam";
 
 export default function Home() {
   return (
+    <div className="min-h-screen bg-white flex justify-center">
+      <div className="mt-400 bg-white px-8 py-8 rounded-2xl shadow-lg border border-gray-200 text-center w-auto h-auto">
+        <h1 className="text-4xl font-bold mb-4 text-black">
+          Website Under Maintenance
+        </h1>
+        <p className="text-lg text-black">
+          We’ll be back soon. Sorry for the inconvenience.
+        </p>
+      </div>
+    </div>
+  );
+  return (
     <>
       <Header />
       <CarouselIntro />
